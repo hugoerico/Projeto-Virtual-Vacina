fb648e761ea1dbad19dcb4a2114c5138d3d8126e
SAsuke019 <hugo.019@hotmail.com>
2020-09-18 19:55:40 -0300

    cabeçalho do site

---
+++ cabecalho.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery.js"></script>
+    <script src="bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Vacina Virtual</title>
+</head>
+<body>
+    <header>
+    <div id="menu1">
+    <nav>   
+    <ul>
+    <img id="logo" src="imagens/logo_carteira1.png" alt="">
+    <li><a href="index.php">Home</a></li>
+    <li>Noticia</li>
+    <li><a href="sobre.php"  rel="noopener noreferrer">Sobre</a></li>
+    </ul>
+    </nav>
+    </div>
+    <div id="menu2">
+    <nav>
+    <ul>
+    <li><a href="cadastro.php">Cadastre-se</a></li>
+    <li><a href="login.php">Login</a></li>
+    </ul>
+    </nav>
+    </div>
+    </header>
+</body>
+</html>
