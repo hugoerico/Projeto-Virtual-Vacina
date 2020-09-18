9daa415ffe0c4a243fc25aa4d99b58a1fbcab7a7
SAsuke019 <hugo.019@hotmail.com>
2020-09-18 19:59:58 -0300

    pagina login do site

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+
+include "cabecalho.php";
+
+?>
+
+
+<section id="login">
+    <h2>Login</h2>
+    <div>
+        <form action="" method="post">
+           <p>
+               <label >
+                CPF:
+                <input type="text" name="cpf" id="">
+            </label>
+        </p>
+        <p>
+            <label >
+                Senha:
+                <input type="passoword" name="senha" id="">
+            </label>
+        </p>
+            <p><a href="http://">Esqueceu sua senha ?</a></p>
+            <p><a href="cadastro.php">Ainda não tem cadastro ?</a></p>
+            
+        </form>
+        
+    </div>
+
+</section>
+
+
+
+<?php
+
+include "rodape.php";
+
+?>
