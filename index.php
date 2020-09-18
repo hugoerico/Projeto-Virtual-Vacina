f545046409384661bbfa304cf0ab168c617a52fc
SAsuke019 <hugo.019@hotmail.com>
2020-09-18 19:57:22 -0300

    index pagina home do site

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include "cabecalho.php";
+?>
+
+<section id="home">
+<div id="baner">
+ <img src="imagens/inf3.png" alt="">
+</div>
+<article>
+<div>
+pppppppppp
+</div>
+<div>
+<img src="imagens/vacinao.png" alt="">
+</div>
+</article>
+</section>
+
+<?php
+include "rodape.php";
+?>
