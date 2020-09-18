8d7fe7d302c1e63f7c2a331121200737e76b1761
SAsuke019 <hugo.019@hotmail.com>
2020-09-18 20:03:37 -0300

    pagina sobre do site

---
+++ sobre.php
@@ -0,0 +1,32 @@
+<?php 
+
+include "cabecalho.php"; 
+
+?>
+
+
+<section id="sobre">
+<h2>Sobre a Vacina Virtual</h2>
+<div id="tudo">
+<div>
+<img src="imagens/vacinao.png" alt="" >
+</div>
+<div>
+
+<p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. A facere magnam, autem nihil doloribus officiis tenetur esse! Minima optio, eveniet nesciunt incidunt neque possimus quasi unde ut aut dolorem culpa?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptatibus culpa blanditiis quasi voluptas veniam recusandae dolores unde earum mollitia, possimus perferendis accusamus doloremque nam quo eaque omnis ea quam.
+</p>
+<p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. A facere magnam, autem nihil doloribus officiis tenetur esse! Minima optio, eveniet nesciunt incidunt neque possimus quasi unde ut aut dolorem culpa?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptatibus culpa blanditiis quasi voluptas veniam recusandae dolores unde earum mollitia, possimus perferendis accusamus doloremque nam quo eaque omnis ea quam.
+</p>
+</div>
+</div>
+</section>
+
+<?php 
+
+include "rodape.php"; 
+
+?>
