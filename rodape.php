f78e331006a62757a47697d50bab716c7a1cc72c
SAsuke019 <hugo.019@hotmail.com>
2020-09-18 20:02:22 -0300

    rodape/footer do site

---
+++ rodape.php
@@ -0,0 +1,16 @@
+<html>
+<link rel="stylesheet" href="style.css">
+
+<footer>
+    <div>
+        <img src="imagens/vacinao.png" alt=""id="vacinao">
+    </div>
+    <div>
+        Site elaborado por Duh e Hugo
+    </div>
+    <div>
+        <img src="imagens/logo_carteira.png" alt="">
+    </div>
+
+</footer>
+</html>
