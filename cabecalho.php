a6d7148bcc0a6095f791df343e860b482030b44b
SAsuke019 <hugo.019@hotmail.com>
2020-09-22 19:40:51 -0300

    adicionando um favicon

--- cabecalho.php
+++ cabecalho.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagens/logo_carteira1.png" type="image/x-icon">
     <script src="jquery.js"></script>
     <script src="bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="bootstrap.min.css">
